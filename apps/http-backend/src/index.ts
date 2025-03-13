809fe9855e2426d99c6edb816975eee0d986eb3e
Abhishek <abhishekkp1721@hotmail.com>
2025-03-13 16:21:29 +0530

    Add starting http backend

---
+++ apps/http-backend/src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import { middleware } from "./middleware";
+
+const app = express();
+
+app.post("/siginin", (req, res) => {
+    // db call here
+
+});
+
+app.post("/signup", (req, res) => {
+
+    // db call here
+
+    const userId = 1;
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
+
+    res.json({
+        token
+    })
+
+});
+
+app.post("/room", middleware, (req, res) => {
+
+    res.json({
+        roomId: 123
+    })
+
+
+});
+
+app.listen(3001);
