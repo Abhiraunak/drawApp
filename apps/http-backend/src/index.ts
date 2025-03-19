7d538a0e9bee78551d5b60b4f211e51587451309
Abhishek <abhishekkp1721@hotmail.com>
2025-03-19 17:55:07 +0530

    add commmon folder

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import { userSchema } from "@repo/common/types";
 import { JWT_SECERET } from "@repo/backend-common/config";
 
 const app = express();
 
 app.post('/signup', (req, res) => {
-
+    const data = userSchema.safeParse(req.body);
+    if (!data.success) {
+        res.status(403).json({
+            message: "Wrong Input"
+        })
+        return;
+    }
 })
 
 app.post('/signin', (req, res) => {
@@ -19,9 +26,15 @@ app.post('/signin', (req, res) => {
         token
     })
 
+    // db call
+
+    res.json({
+        userId: "123"
+    })
+
 })
 
 app.post('/room', (req, res) => {
-    
+
 })
 app.listen(3000);
